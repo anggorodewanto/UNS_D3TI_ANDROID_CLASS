21cea43009bf13d5a1a1b017169ec3e5f8e940f8
yuliana22 <yulianaagustiningsih@gmail.com>
2015-09-15 00:03:33 +0700

    taskbutton

--- StudentProjects/2015/Individual/M3113152_YULIANA-AGUSTININGSIH/1_Button_Click/app/src/main/java/com/example/windows7/button_click/MainActivity.java
+++ StudentProjects/2015/Individual/M3113152_YULIANA-AGUSTININGSIH/1_Button_Click/app/src/main/java/com/example/windows7/button_click/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.windows7.task_view;
+package com.example.windows7.button_click;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -45,3 +46,4 @@ public class MainActivity extends Activity {
 
     }
 }
+
