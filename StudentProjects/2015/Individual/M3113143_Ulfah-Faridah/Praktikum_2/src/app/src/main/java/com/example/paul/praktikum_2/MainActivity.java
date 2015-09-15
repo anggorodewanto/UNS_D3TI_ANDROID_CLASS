5b1727136a63eac50df4467c3a38f507fd6ee0ee
unknown <ulpah.farida@gmail.com>
2015-09-15 14:48:02 +0700

    @belumjadi

--- StudentProjects/2015/Individual/M3113143_Ulfah-Faridah/Praktikum_2/src/app/src/main/java/com/example/paul/praktikum_2/MainActivity.java
+++ StudentProjects/2015/Individual/M3113143_Ulfah-Faridah/Praktikum_2/src/app/src/main/java/com/example/paul/praktikum_2/MainActivity.java
@@ -1,37 +1,59 @@
 package com.example.paul.praktikum_2;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.app.Activity;
+import android.graphics.Color;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void add(View v)
+    {
+        LinearLayout ll=(LinearLayout)findViewById(R.id.ll);
+        TextView result=(TextView)findViewById(R.id.result);
+        EditText et1=(EditText)findViewById(R.id.editText1);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //get value from edit text box and convert into double
+        double a=Double.parseDouble(String.valueOf(et1.getText()));
+        RadioButton cb=(RadioButton)findViewById(R.id.cb);
+        RadioButton fb=(RadioButton)findViewById(R.id.fb);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        //check which radio button is checked
+        if(cb.isChecked())
+        {
+            //change background colour
+            ll.setBackgroundColor(Color.YELLOW);
+            //display conversion
+            result.setText(f2c(a)+" degree C");
+            //cb.setChecked(false);
+            fb.setChecked(true);
         }
-
-        return super.onOptionsItemSelected(item);
+        else
+        {
+            ll.setBackgroundColor(Color.CYAN);
+            result.setText(c2f(a)+" degree F");
+            //fb.setChecked(false);
+            cb.setChecked(true);
+        }
+    }
+    //Celcius to Fahrenhiet method
+    private double c2f(double c)
+    {
+        return (c*9)/5+32;
+    }
+    //Fahrenhiet to Celcius method
+    private double f2c(double f)
+    {
+        return (f-32)*5/9;
     }
 }
