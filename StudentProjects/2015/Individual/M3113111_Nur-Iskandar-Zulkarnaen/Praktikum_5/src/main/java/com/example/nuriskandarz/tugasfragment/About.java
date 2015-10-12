e2cd148c9d89b3f1fc6a8a84a356b20c70d56770
nuriskandarzulkarnaen <nuriskandarzulkarnaen@gmail.com>
2015-10-13 03:13:06 +0700

    praktikum_5
    praktikum_5

---
+++ StudentProjects/2015/Individual/M3113111_Nur-Iskandar-Zulkarnaen/Praktikum_5/src/main/java/com/example/nuriskandarz/tugasfragment/About.java
@@ -0,0 +1,19 @@
+package com.example.nuriskandarz.tugasfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Nur Iskandar Z on 13/10/2015.
+ */
+public class About extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.about, container, false);
+    }
+}
