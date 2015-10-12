f758c40bda47ee090097af41d43a69ba20a499c7
Obeth Dewa Wardhana <obedewardhana@gmail.com>
2015-10-12 21:36:07 +0700

    +Added : M3113112-fragment

---
+++ StudentProjects/2015/Individual/M3113112_Obeth-Dewa-Wardhana/M3113112_fragment/main/java/M3113112_ObethDewa/fragments/Fragment3.java
@@ -0,0 +1,19 @@
+package m3113123_ridwan_yoga_pradana.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HP on 10/12/2015.
+ */
+public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment3, container, false);
+    }
+}
