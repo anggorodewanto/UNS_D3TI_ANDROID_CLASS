bc604d28ddfae36a1fc4142ddbc86c74e9f0c592
muawana <smuawana18@gmail.com>
2015-10-13 10:47:52 +0700

    Praktikum5_fragment

---
+++ StudentProjects/2015/Individual/M3113136_SITI-MUAWANA/Praktikum5_fragment/src/main/java/com/example/muawana/fragment/citacita.java
@@ -0,0 +1,19 @@
+package com.example.muawana.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 10/12/15.
+ */
+public class citacita extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.citacita,container,false);
+    }
+}
