9d704e55acd6457b48c820a5bf9c42e1f69d6fc7
mahfudz <odik.wodota@gmail.com>
2015-10-15 09:22:18 +0700

    tugas_materi_kelompok3

---
+++ StudentProjects/2015/Individual/M3113102_Mochamad-Mahfudz-S/Tugas_materi_kelempok3/src/main/java/com/example/hp/tugas_kelompok_3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hp.tugas_kelompok_3;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
