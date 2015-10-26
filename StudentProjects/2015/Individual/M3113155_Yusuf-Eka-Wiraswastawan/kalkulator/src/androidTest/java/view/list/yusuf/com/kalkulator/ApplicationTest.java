741c9b98d84b50979cdf019ca08033da304127e3
yusuf <ucupz.caem@gmail.com>
2015-10-26 20:07:07 +0700

    mm

---
+++ StudentProjects/2015/Individual/M3113155_Yusuf-Eka-Wiraswastawan/kalkulator/src/androidTest/java/view/list/yusuf/com/kalkulator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package view.list.yusuf.com.kalkulator;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
