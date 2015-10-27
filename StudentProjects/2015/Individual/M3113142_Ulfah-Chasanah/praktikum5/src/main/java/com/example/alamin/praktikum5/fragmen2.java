2468ab1699a6569e59dc6ad425c5b1663e142246
ulfahchasanah <chasanahulfah1995@gmail.com>
2015-10-27 13:54:29 +0700

    praktikum5

---
+++ StudentProjects/2015/Individual/M3113142_Ulfah-Chasanah/praktikum5/src/main/java/com/example/alamin/praktikum5/fragmen2.java
@@ -0,0 +1,18 @@
+package com.example.alamin.praktikum5;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Al'amin on 15/10/2015.
+ */
+public class fragmen2 extends fragmen1 {
+    @Nullable
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragmen1,container,false);
+    }
+}
