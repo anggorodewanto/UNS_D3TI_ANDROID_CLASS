d7bd69e38d76adcaa232ff227fc1b80db9e9a7bb
willfaarmila123 <willfaarmilaputeri@gmail.com>
2015-11-01 13:49:45 +0700

    TUGAS7
    THEME

---
+++ StudentProjects/2015/Individual/M3113148_Willfa-Armila-Puteri/TUGASTHEME7/app/src/main/java/com/example/willfa/tugastheme/CustomView.java
@@ -0,0 +1,30 @@
+package com.example.willfa.tugastheme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by willfa on 31/10/2015.
+ */
+
+public class CustomView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_view);
+
+        Button btnback = (Button) findViewById(R.id.button1);
+
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivity(new Intent(CustomView.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
