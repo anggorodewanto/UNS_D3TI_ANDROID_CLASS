c779b17ae012128dd49fa1442731933e1c4732eb
muhammadreza <reza_separo29@yahoo.co.id>
2015-11-02 08:14:12 +0700

    Theme

---
+++ StudentProjects/2015/Individual/M3113105_Muhammad_Reza/Theme/src/main/java/com/example/alone/theme/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.alone.theme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alone on 11/1/2015.
+ */
+public class MainActivity2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+    }
+}
