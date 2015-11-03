4000ac176f127b04969b8fb07653feaea5aac39a
Ganis Sayoga <ganisayoga@gmail.com>
2015-11-02 12:15:31 -1200

    Prak_kasus4

---
+++ StudentProjects/2015/Individual/M3113064_GANIS-SAYOGA/prak_kasus4_listview/src/androidTest/java/com/example/ganis/tugas_topik4_m3113064/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.ganis.tugas_topik4_m3113064;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
