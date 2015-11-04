19d9343824f0a3eaa25e0082405bd14b69cce256
Intan Nur Azizah <intanna7@gmail.com>
2015-11-04 09:41:18 +0700

    tugas kelompok 5
    ini tugas kelompok 5 pak :)

---
+++ StudentProjects/2015/Individual/M3113080_INTAN-NUR-AZIZAH/Tugas_kelompok5/src/main/java/com/example/user/tugas_topik5/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.user.tugas_topik5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+    TabHost tabHost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Call");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Call");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Chat");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Chat");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Contak");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Contact");
+        host.addTab(spec);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
