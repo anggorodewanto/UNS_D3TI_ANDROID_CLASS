e69e1bdacd986b32dc384f93bde26ffe20220a77
adiozh <adiozh@gmail.com>
2015-11-06 18:18:44 +0700

    Materi WIdget
    materi widget kelompok 4 TIF

---
+++ StudentProjects/2015/Group/TI_EF/KELOMPOK_4/src/main/java/com/example/setiadidarmawan/widgetadiozh/Widgetjava.java
@@ -0,0 +1,16 @@
+package com.example.setiadidarmawan.widgetadiozh;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Setiadi Darmawan on 03-Nov-15.
+ */
+public class Widgetjava extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+    }
+}
