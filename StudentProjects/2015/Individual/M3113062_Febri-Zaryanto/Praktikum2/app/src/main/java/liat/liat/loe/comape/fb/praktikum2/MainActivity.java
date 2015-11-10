86b295efdcf9437242d851c6c6239b7c760f411b
muhammadreza <reza_separo29@yahoo.co.id>
2015-11-10 23:08:58 +0700

    change

---
+++ StudentProjects/2015/Individual/M3113062_Febri-Zaryanto/Praktikum2/app/src/main/java/liat/liat/loe/comape/fb/praktikum2/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.praktikum2;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import liat.liat.loe.comape.fb.praktikum2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView title = new TextView(this);
+        final EditText input = new EditText(this);
+        Button buton = new Button(this);
+        final TextView hasil = new TextView(this);
+
+        title.setText("Konversi Celcius ke Fahrenheit");
+        title.setTextSize(14);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setGravity(Gravity.CENTER);
+        buton.setGravity(Gravity.CENTER);
+        buton.setText("Submit");
+
+        LinearLayout tampil = new LinearLayout(this);
+
+        tampil.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        title.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        input.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        buton.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        tampil.setOrientation(LinearLayout.VERTICAL);
+        tampil.setGravity(Gravity.CENTER);
+
+        tampil.addView(title);
+        tampil.addView(input);
+        tampil.addView(buton);
+        tampil.addView(hasil);
+
+        buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float angka = Float.parseFloat(input.getText().toString());
+                hasil.setText(String.valueOf(angka * 9 / 5 + 32) + " °F");
+            }
+        });
+
+        setContentView(tampil);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ StudentProjects/2015/Individual/M3113066_GIOVANNI-ARTHANDIO-YUDHA-PANGESTU/Tugas Kelompok 1/src/main/java/com/example/win7/tgskelompok1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.win7.tgskelompok1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity{
+    public static final int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOpen = (Button) findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                callIntent();
+            }
+        });
+    }
+
+    public void callIntent() {
+        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+        EditText text1 = (EditText) findViewById(R.id.editText1);
+        String dataactivity_1 = text1.getText().toString();
+        //untuk mengirim data gunakan putExtra
+        i.putExtra("value1", dataactivity_1);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("return_value1")) {
+                EditText text2 = (EditText) findViewById(R.id.editTextdr2);
+                text2.setText(data.getExtras().getString("return_value1"));
+            }
+        }
+    }
+}
