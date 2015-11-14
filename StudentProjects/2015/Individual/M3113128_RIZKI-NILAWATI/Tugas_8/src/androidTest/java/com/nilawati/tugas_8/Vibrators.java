ee44583de9b7854c31b30a59684ffadfb7af643a
rizkinilawati <rizkinila@gmail.com>
2015-11-15 05:37:18 +0700

    Praktikum_8

---
+++ StudentProjects/2015/Individual/M3113128_RIZKI-NILAWATI/Tugas_8/src/androidTest/java/com/nilawati/tugas_8/Vibrators.java
@@ -0,0 +1,55 @@
+package com.nilawati.tugas_8;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.RemoteViews;
+
+/**
+ * Created by Rizki Nilawati on 13/11/2015.
+ */
+public class Vibrators extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    public void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        final Vibrator vibe = (Vibrator)
+                context.getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        Intent intent = new Intent(context, Vibrators.class);
+        intent.setAction(appWidgetManager.ACTION_APPWIDGET_UPDATE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.vibrator);
+
+        views.setOnClickPendingIntent(R.id.button, pendingIntent);
+        vibe.vibrate(100);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+}
