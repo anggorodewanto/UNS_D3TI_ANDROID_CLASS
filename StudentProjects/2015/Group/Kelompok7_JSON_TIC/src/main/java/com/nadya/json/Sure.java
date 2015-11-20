cef1e00ae5ac25c3679db885c8329c034cdd9bfe
nadsitohang <ns200995@gmail.com>
2015-11-20 17:13:22 +0700

    +Added : Kelompok7_JSON_TIEF

---
+++ StudentProjects/2015/Group/Kelompok7_JSON_TIC/src/main/java/com/nadya/json/Sure.java
@@ -0,0 +1,160 @@
+package com.nadya.json;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+//import com.tutorialspoint.myapplication.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Sure extends ActionBarActivity {
+
+    private ProgressDialog progressDialog;
+    private Bitmap bitmap = null;
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.tombol);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkInternetConenction();
+                downloadImage("https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xfp1/t31.0-1/c436.94.1177.1177/s960x960/904263_527216150653702_2139668936_o.jpg");
+            }
+        });
+    }
+
+    private void downloadImage(String urlStr) {
+        progressDialog = ProgressDialog.show(this, "", "Downloading Image from " + urlStr);
+        final String url = urlStr;
+
+        new Thread() {
+            public void run() {
+                InputStream in = null;
+
+                Message msg = Message.obtain();
+                msg.what = 1;
+
+                try {
+                    in = openHttpConnection(url);
+                    bitmap = BitmapFactory.decodeStream(in);
+                    Bundle b = new Bundle();
+                    b.putParcelable("bitmap", bitmap);
+                    msg.setData(b);
+                    in.close();
+                }
+
+                catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                messageHandler.sendMessage(msg);
+            }
+        }.start();
+    }
+
+    private InputStream openHttpConnection(String urlStr) {
+        InputStream in = null;
+        int resCode = -1;
+
+        try {
+            URL url = new URL(urlStr);
+            URLConnection urlConn = url.openConnection();
+
+            if (!(urlConn instanceof HttpURLConnection)) {
+                throw new IOException("URL is not an Http URL");
+            }
+            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            resCode = httpConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return in;
+    }
+
+    private Handler messageHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            ImageView img = (ImageView) findViewById(R.id.tampil);
+            img.setImageBitmap((Bitmap) (msg.getData().getParcelable("bitmap")));
+            progressDialog.dismiss();
+        }
+    };
+
+    private boolean checkInternetConenction() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager connec =(ConnectivityManager)getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+
+        // Check for network connections
+        if ( connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ) {
+            Toast.makeText(this, " Connected ", Toast.LENGTH_LONG).show();
+            return true;
+        }else if (
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                        connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
+            Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
