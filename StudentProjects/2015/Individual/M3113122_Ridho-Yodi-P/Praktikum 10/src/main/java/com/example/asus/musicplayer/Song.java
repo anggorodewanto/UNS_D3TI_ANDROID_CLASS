cee5aa890db4b31b42c21f818e71ec250bb99f35
ridhodikas <ridho4pm@gmail.com>
2015-11-21 21:27:25 +0700

    Praktikum 10
    Media Player

---
+++ StudentProjects/2015/Individual/M3113122_Ridho-Yodi-P/Praktikum 10/src/main/java/com/example/asus/musicplayer/Song.java
@@ -0,0 +1,22 @@
+package com.example.asus.musicplayer;
+
+/**
+ * Created by asus on 21/11/2015.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
