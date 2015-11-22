e75c53a04c5dd8fe664be3c327650e4857e2b3e4
Wahyuning Tri Utami <utamiw6@gmail.com>
2015-11-23 05:12:34 +0700

    Tugas Media Player

---
+++ StudentProjects/2015/Individual/M3113145_WAHYUNING-TRI-UTAMI/TugasMediaPlayer/app/src/main/java/tugas/github/tugasmediaplayer/MusicController.java
@@ -0,0 +1,16 @@
+package tugas.github.tugasmediaplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by TOSHIBA on 23/11/2015.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
