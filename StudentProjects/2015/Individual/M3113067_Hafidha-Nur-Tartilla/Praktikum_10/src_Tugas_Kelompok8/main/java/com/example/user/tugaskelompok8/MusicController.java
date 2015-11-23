73c928e7be4a05264698c9b064979daccc0a7bca
hafidhanurtartilla <hafidhanurtartilla@gmail.com>
2015-11-24 03:19:24 +0700

    Tugas Kelompok 8

---
+++ StudentProjects/2015/Individual/M3113067_Hafidha-Nur-Tartilla/Praktikum_10/src_Tugas_Kelompok8/main/java/com/example/user/tugaskelompok8/MusicController.java
@@ -0,0 +1,18 @@
+package com.example.user.tugaskelompok8;
+
+/**
+ * Created by User on 23/11/2015.
+ */
+import android.content.Context;
+import android.widget.MediaController;
+
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
