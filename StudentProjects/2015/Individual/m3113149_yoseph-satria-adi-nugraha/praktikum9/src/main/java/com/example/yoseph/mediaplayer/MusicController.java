e4b6539582ac2bb23141e88c61b335f4ac6d2ac4
yosepsatria <yosephsatria1@gmail.com>
2015-11-23 08:52:18 +0700

    praktikum8 praktikum9
    widgetgetar
    mediaplayer

---
+++ StudentProjects/2015/Individual/m3113149_yoseph-satria-adi-nugraha/praktikum9/src/main/java/com/example/yoseph/mediaplayer/MusicController.java
@@ -0,0 +1,15 @@
+package com.example.yoseph.mediaplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by yoseph on 11/13/2015.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
---
+++ StudentProjects/2015/Individual/m3113149_yoseph-satria-adi-nugraha/praktikum9/src/main/java/com/example/yoseph/mediaplayer/Song.java
@@ -0,0 +1,23 @@
+package com.example.yoseph.mediaplayer;
+
+/**
+ * Created by yoseph on 11/13/2015.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
+
