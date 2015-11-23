f06dc416553401621d4787941694963a4b47acc1
Intan Nur Azizah <intanna7@gmail.com>
2015-11-23 17:29:10 +0700

    Tugas kelompok 8
    ini tugas kelompok 8 pak :)

---
+++ StudentProjects/2015/Individual/M3113080_INTAN-NUR-AZIZAH/Tugas_kelompok8/src/main/java/com/example/user/tugas_topik8/Song.java
@@ -0,0 +1,20 @@
+package com.example.user.tugas_topik8;
+
+/**
+ * Created by User on 22/11/2015.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
