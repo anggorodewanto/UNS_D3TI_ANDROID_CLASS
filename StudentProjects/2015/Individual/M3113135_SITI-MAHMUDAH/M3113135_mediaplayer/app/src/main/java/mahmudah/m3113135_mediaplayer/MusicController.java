9c24786060f69964eaee3dfd6f2f15d2c4c7df81
mahmudmahmudah <s.mahmudah89@gmail.com>
2015-11-23 16:14:43 +0700

    M3113135_Tugas_MediaPlayer

---
+++ StudentProjects/2015/Individual/M3113135_SITI-MAHMUDAH/M3113135_mediaplayer/app/src/main/java/mahmudah/m3113135_mediaplayer/MusicController.java
@@ -0,0 +1,13 @@
+package mahmudah.m3113135_mediaplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
