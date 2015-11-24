cb382f7fd7207e2f10bcfa2cb0c6625abf577901
nadsitohang <ns200995@gmail.com>
2015-11-24 23:56:24 +0700

    +Added :  Tugas_10

---
+++ StudentProjects/2015/Individual/M3113107_Nadya_Tiurma_W_S/Tugas_10/src/main/java/com/nadya/player/MusicController.java
@@ -0,0 +1,18 @@
+package com.nadya.player;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Nadya on 17/11/2015.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+
+}
