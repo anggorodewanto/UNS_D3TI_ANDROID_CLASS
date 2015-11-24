1d68adbabd6c4f76c4558e5893961b8d8f79f157
R Bella Husada <bella.husada@gmail.com>
2015-11-24 23:52:04 +0700

    + Added: Praktikum_10 (Media Player)

---
+++ StudentProjects/2015/Individual/M3113116_Ramadhani_Bella_Husada/Praktikum_10/src/main/java/com/example/bella/praktikum_10/MusicController.java
@@ -0,0 +1,15 @@
+package com.example.bella.praktikum_10;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by User-TOSHIBA on 11/24/2015.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
