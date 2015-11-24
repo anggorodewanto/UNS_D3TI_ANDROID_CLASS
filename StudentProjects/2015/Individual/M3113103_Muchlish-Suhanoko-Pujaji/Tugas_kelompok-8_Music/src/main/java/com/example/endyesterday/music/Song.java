a30e5c66a79d1fac8d6986a337191ee87e58dd06
ucisa <much.ucis@gmail.com>
2015-11-25 00:54:52 +0700

    Tugas_Kelompok-8_Music

---
+++ StudentProjects/2015/Individual/M3113103_Muchlish-Suhanoko-Pujaji/Tugas_kelompok-8_Music/src/main/java/com/example/endyesterday/music/Song.java
@@ -0,0 +1,22 @@
+package com.example.endyesterday.music;
+
+/**
+ * Created by EndYesterday on 24-Nov-15.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
