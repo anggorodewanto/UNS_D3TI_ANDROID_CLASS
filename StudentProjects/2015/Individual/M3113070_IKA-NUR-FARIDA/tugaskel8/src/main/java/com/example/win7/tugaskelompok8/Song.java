b5daffdf93143638e6091c72aaf79832e3308df2
Ikanurfarida <nurikafarida@gmail.com>
2015-11-27 08:17:16 -0800

    tugas 7 dan 8

---
+++ StudentProjects/2015/Individual/M3113070_IKA-NUR-FARIDA/tugaskel8/src/main/java/com/example/win7/tugaskelompok8/Song.java
@@ -0,0 +1,22 @@
+package com.example.win7.tugaskelompok8;
+
+/**
+ * Created by user on 25/11/2015.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
