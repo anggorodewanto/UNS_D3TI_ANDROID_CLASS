40f67201d6907eb1dbc5d303457535d0f71a861f
Kholida <ida.liliput@gmail.com>
2015-11-30 21:38:45 +0700

    Kelompok 10

---
+++ StudentProjects/2015/Group/Kelompok_3/BatteryLevel-Brodadcast-K10/app/src/main/java/com/example/se7en/batterylevel_brodadcast_k10/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.se7en.batterylevel_brodadcast_k10;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mBatteryLevelText;
+    private ProgressBar mBatteryLevelProgress;
+    private BroadcastReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBatteryLevelText = (TextView) findViewById(R.id.textView);
+        mBatteryLevelProgress = (ProgressBar) findViewById(R.id.progressBar);
+        mReceiver = new BatteryBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        unregisterReceiver(mReceiver);
+        super.onStop();
+    }
+    private class BatteryBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            mBatteryLevelText.setText(getString(R.string.battery_level) + " " + level);
+            mBatteryLevelProgress.setProgress(level);
+        }
+    }
+}
