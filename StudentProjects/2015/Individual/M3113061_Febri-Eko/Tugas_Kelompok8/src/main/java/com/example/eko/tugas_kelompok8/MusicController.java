063c563cb45889bf7cf7fce7886185ecff9fb4cb
febriekosaputro <eko141@student.uns.ac.id>
2015-12-01 16:38:34 +0700

    Tugas_Kelompok8
    Tugas_Kelompok8

---
+++ StudentProjects/2015/Individual/M3113061_Febri-Eko/Tugas_Kelompok8/src/main/java/com/example/eko/tugas_kelompok8/MusicController.java
@@ -0,0 +1,15 @@
+package com.example.eko.tugas_kelompok8;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Eko on 12/1/2015.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
