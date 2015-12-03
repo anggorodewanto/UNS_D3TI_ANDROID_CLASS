e3d74fc51ad494ecc476b25aafb33ff808c74190
INSANI NUR HIDAYAH <insaninur7@gmail.com>
2015-12-03 11:01:04 -0800

    TugasTopik10

---
+++ StudentProjects/2015/Individual/M3113078_INSANI-NUR-HIDAYAH/TUGASTOPIK10/src/main/java/com/example/asus8/tugastopik10/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package com.example.asus8.tugastopik10;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+/**
+ * Created by asus 8 on 12/02/2015.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        Toast.makeText(context, "Alarm aktif!", Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.alarm);
+        player.start();
+    }
+
+}
