2717c3fbbc1a9a251b5b382cbd19b0b8b87aa2a2
rizkinilawati <rizkinila@gmail.com>
2015-12-03 14:41:00 +0700

    Praktikum_10

---
+++ StudentProjects/2015/Individual/M3113128_RIZKI-NILAWATI/Praktikum_10/src/main/java/com/nilawati/mediaplayer/Song.java
@@ -0,0 +1,20 @@
+package com.nilawati.mediaplayer;
+
+/**
+ * Created by Rizki Nilawati on 21/11/2015.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
