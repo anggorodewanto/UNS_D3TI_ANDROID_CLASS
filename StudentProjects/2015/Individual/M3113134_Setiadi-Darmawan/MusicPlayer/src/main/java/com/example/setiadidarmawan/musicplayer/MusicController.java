7acab42c37842eb618f240bbdc7411f36941109c
adiozh <adiozh@gmail.com>
2015-12-08 22:28:36 +0700

    Music Player

---
+++ StudentProjects/2015/Individual/M3113134_Setiadi-Darmawan/MusicPlayer/src/main/java/com/example/setiadidarmawan/musicplayer/MusicController.java
@@ -0,0 +1,18 @@
+package com.example.setiadidarmawan.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Setiadi Darmawan on 25-Nov-15.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
+
+
