e8fc495a539995a46dfce18023b33871ca2ff602
juanaldoalfreda <juanaldoalfreda@gmail.com>
2015-12-16 12:14:00 +0700

    tugas

---
+++ StudentProjects/2015/Individual/M3113084_Juanaldo-Alfreda/tugaskelompok9/src/main/java/com/example/user/tugaskelompok9/newKampus.java
@@ -0,0 +1,38 @@
+package com.example.user.tugaskelompok9;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashMap;
+
+/**
+ * Created by USER on 03/12/2015.
+ */
+public class newKampus extends Activity {
+    EditText kampusName,alamat;
+    dbController controller = new dbController(this);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_kampus);
+        kampusName = (EditText) findViewById(R.id.txtKampusName);
+        alamat = (EditText) findViewById(R.id.txtAlamat);
+    }
+    /* kode untuk menambah data kampus*/
+    public void addNewKampus(View view) {
+        HashMap<String, String> queryValues =  new  HashMap<String, String>();
+        queryValues.put("kampusName", kampusName.getText().toString());
+        queryValues.put("alamat", alamat.getText().toString());
+        controller.addKampus(queryValues);
+        this.callHomeActivity(view);
+    }
+    /* panggil MainActivity */
+    public void callHomeActivity(View view) {
+        Intent objIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(objIntent);
+    }
+}
