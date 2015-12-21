0daea9d171e56ac5990550733ba446c2749559fa
finastiana <fardila@student.uns.ac.id>
2015-12-21 22:33:48 +0700

    TugasKelompok8

---
+++ StudentProjects/2015/Individual/M3113060_Fardila-Inastiana/TugasKelompok8/app/src/main/java/com/example/admin/tugaskelompok8/MusicController.java
@@ -0,0 +1,20 @@
+package com.example.admin.tugaskelompok8;
+
+/**
+ * Created by Admin on 12/21/2015.
+ */
+import android.content.Context;
+import android.widget.MediaController;
+
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
+
+
