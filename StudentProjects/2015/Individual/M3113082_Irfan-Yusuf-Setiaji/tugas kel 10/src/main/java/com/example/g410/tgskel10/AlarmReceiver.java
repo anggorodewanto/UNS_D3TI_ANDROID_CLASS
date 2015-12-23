16cbb6417ddcfd4b015a31bc8851cfa2866f8ca6
irfanyusuf <irfan.setiaji777@gmail.com>
2015-12-23 21:38:28 +0700

    tugas 8 - 11
    tugas 8 - 11

---
+++ StudentProjects/2015/Individual/M3113082_Irfan-Yusuf-Setiaji/tugas kel 10/src/main/java/com/example/g410/tgskel10/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package com.example.g410.tgskel10;
+
+/**
+ * Created by G410 on 23/12/2015.
+ */
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        Toast.makeText(context, "Alarm aktif!", Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.alarm);
+        player.start();
+    }
+
+}
---
+++ StudentProjects/2015/Individual/M3113082_Irfan-Yusuf-Setiaji/tugas kel 8/src/main/java/com/example/g410/tgskel8/MusicController.java
@@ -0,0 +1,20 @@
+package com.example.g410.tgskel8;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by G410 on 23/12/2015.
+ */
+
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
+
