e18e9f2f245ce539b7c41537177d5f3194dff079
luthfihidayati <luthfihidayati6@gmail.com>
2015-12-23 10:26:04 +0700

    tugas kelompok 8,9,10,11

---
+++ StudentProjects/2015/Individual/M3113092_LUTHFI-HIDAYATI/kel10/src/main/java/com/example/user/tugaskelompok10/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package com.example.user.tugaskelompok10;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+/**
+ * Created by USER on 04/12/2015.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    MediaPlayer player;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        Toast.makeText(context, "Alarm aktif!", Toast.LENGTH_LONG).show();
+        player = MediaPlayer.create(context, R.raw.alarm);
+        player.start();
+    }
+
+}
