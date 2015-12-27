498ab92416abedda842d63945a227e7727b0fad7
Lintang Panjerina <panjerinstar@gmail.com>
2015-12-27 21:14:14 +0700

    Tugas Kelompok 8,9,10,11

---
+++ StudentProjects/2015/Individual/M3113090_Lintang-Panjerina/Tugas Kelompok 8,9,10,11/Kelompok 8/src/main/java/com/example/galaxy/tugaskelompok8/MusicController.java
@@ -0,0 +1,19 @@
+package com.example.galaxy.tugaskelompok8;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by user on 14/11/2015.
+ */
+
+
+    public class MusicController extends MediaController {
+
+        public MusicController(Context c){
+            super(c);
+        }
+
+        public void hide(){}
+
+    }
