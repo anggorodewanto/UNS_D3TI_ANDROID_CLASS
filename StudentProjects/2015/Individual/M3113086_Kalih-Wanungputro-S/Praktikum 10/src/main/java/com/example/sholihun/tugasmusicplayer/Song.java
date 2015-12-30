58c439b129f32700f972943a1fb86106e82758ba
kalih <kalihwanungputro@gmail.com>
2015-12-30 08:10:42 +0700

    Tugas MusicPlayer, Database, Alarm

---
+++ StudentProjects/2015/Individual/M3113086_Kalih-Wanungputro-S/Praktikum 10/src/main/java/com/example/sholihun/tugasmusicplayer/Song.java
@@ -0,0 +1,19 @@
+package com.example.sholihun.tugasmusicplayer;
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
---
+++ StudentProjects/2015/Individual/M3113086_Kalih-Wanungputro-S/Praktikum 11/src/main/java/com/example/sholihun/tugasdatabase/EditKampusActivity.java
@@ -0,0 +1,60 @@
+package com.example.sholihun.tugasdatabase;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.HashMap;
+
+public class EditKampusActivity extends Activity{
+    EditText txtKampusName,txtEditAlamat;
+    DBController controller = new DBController(this);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_kampus);
+        txtKampusName = (EditText) findViewById(R.id.txtKampusName);
+        txtEditAlamat = (EditText) findViewById(R.id.txtEditAlamat);
+        Intent objIntent = getIntent();
+        String kampusId = objIntent.getStringExtra("kampusId");
+        Log.d("Reading: ", "Reading all kampus..");
+        HashMap<String, String> kampusList = controller.getKampusInfo(kampusId);
+        Log.d("kampusName",kampusList.get("kampusName"));
+        if(kampusList.size()!=0) {
+            txtKampusName.setText(kampusList.get("kampusName"));
+            txtEditAlamat.setText(kampusList.get("alamat"));
+        }
+    }
+    public void editKampus(View view) {
+        HashMap<String, String> queryValues =  new  HashMap<String, String>();
+        txtKampusName = (EditText) findViewById(R.id.txtKampusName);
+        txtEditAlamat = (EditText) findViewById(R.id.txtEditAlamat);
+        Intent objIntent = getIntent();
+        String kampusId = objIntent.getStringExtra("kampusId");
+        queryValues.put("kampusId", kampusId);
+        queryValues.put("kampusName", txtKampusName.getText().toString());
+        queryValues.put("alamat", txtEditAlamat.getText().toString());
+
+        controller.updateKampus(queryValues);
+        this.callHomeActivity(view);
+
+    }
+    public void removeKampus(View view) {
+        Intent objIntent = getIntent();
+        String kampusId = objIntent.getStringExtra("kampusId");
+        controller.deleteKampus(kampusId);
+        this.callHomeActivity(view);
+
+    }
+    public void callHomeActivity(View view) {
+        Intent objIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(objIntent);
+    }
+}
+
+
+
+
