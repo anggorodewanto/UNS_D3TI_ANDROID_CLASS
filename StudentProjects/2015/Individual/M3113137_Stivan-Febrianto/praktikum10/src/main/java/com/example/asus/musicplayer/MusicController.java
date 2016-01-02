a8c17ac39a17cc9870d3835e906738e07f26ce73
Stivan Febrianto <stivan.febriant@gmail.com>
2016-01-02 09:38:05 +0700

    praktikum9-13

---
+++ StudentProjects/2015/Individual/M3113137_Stivan-Febrianto/praktikum10/src/main/java/com/example/asus/musicplayer/MusicController.java
@@ -0,0 +1,15 @@
+package com.example.asus.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by asus on 21/11/2015.
+ */
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
---
+++ StudentProjects/2015/Individual/M3113137_Stivan-Febrianto/praktikum11/src/main/java/stivan/databasesql/Student.java
@@ -0,0 +1,18 @@
+package stivan.databasesql;
+
+
+public class Student {
+    public static final String TABLE = "Student";
+
+    // Labels Table Columns names
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_email = "email";
+    public static final String KEY_age = "age";
+
+    // property help us to keep data
+    public int student_ID;
+    public String name;
+    public String email;
+    public int age;
+}
