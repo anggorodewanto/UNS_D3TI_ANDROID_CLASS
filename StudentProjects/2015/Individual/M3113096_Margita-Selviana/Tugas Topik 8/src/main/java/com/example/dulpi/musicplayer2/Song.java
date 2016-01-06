dff56cdd44a64b7746ea814ae2a52fb295f0d9a4
unknown <selvianamargita@gmail.com>
2016-01-06 12:31:55 +0700

    8,9,10,11 M3113096

---
+++ StudentProjects/2015/Individual/M3113096_Margita-Selviana/Tugas Topik 8/src/main/java/com/example/dulpi/musicplayer2/Song.java
@@ -0,0 +1,22 @@
+package com.example.dulpi.musicplayer2;
+
+/**
+ * Created by user on 14/11/2015.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
